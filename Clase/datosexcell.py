a52761f289e05e7d8732100ffa397ba43536d194
Brandon Quintero <developerbvth@gmail.com>
2023-08-03 14:41:17 -0500

    examen Estadistica Descriptiva

--- Clase/datosexcell.py
+++ Clase/datosexcell.py
@@ -20,5 +20,23 @@ print("valor minimo", min(num))
 print("valor sumado", sum(num))
 print("valor media", num.mean())
 print("valor std", num.std())
-print("valor std", num.dot())
+print("valor nose", num.dot())
+# Promedio
+# Datos de ejemplo
+datos = np.array([1, 2, 3, 4, 4, 5, 6, 7, 8, 8, 8])
+
+
+promedio = np.mean(datos)
+
+# Moda
+moda = np.argmax(np.bincount(datos))
+
+# Mediana
+mediana = np.median(datos)
+
+# Varianza
+varianza = np.var(datos, ddof=0)  # ddof=0 para usar la población completa
+
+# Desviación estándar
+desviacion_estandar = np.std(datos, ddof=0)  # ddof=0 para usar la población completa
 
